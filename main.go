627761c525c72d23a9a8e04de5404cace74f26b8
Ridho Febriansa <officialridhof@gmail.com>
2021-02-23 13:56:22 +0700

    fix: refactor main.go by using routes package

--- main.go
+++ main.go
@@ -1,21 +1,12 @@
 package main
 
 import (
-	"echo-user/controllers"
 	"echo-user/config"
-
-	"github.com/labstack/echo"
+	"echo-user/routes"
 )
 
 func main() {
 	config.InitDB()
-
-	e := echo.New()
-	e.GET("/users", controllers.GetUsersController)
-	e.GET("/users/:id", controllers.GetUserController)
-	e.POST("/users", controllers.CreateUserController)
-	e.DELETE("/users/:id", controllers.DeleteUserController)
-	e.PUT("/users/:id", controllers.UpdateUserController)
-
+	e := routes.New()
 	e.Logger.Fatal(e.Start(":8000"))
 }
