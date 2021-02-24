6eedb54031ac23f51e7b69c1926faa6fc6abe1b1
Ridho Febriansa <officialridhof@gmail.com>
2021-02-24 11:58:43 +0700

    fix: use log middleware from middlewares package

--- main.go
+++ main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"echo-user/config"
 	"echo-user/routes"
+	m "echo-user/middlewares"
 )
 
 func main() {
 	config.InitDB()
 	e := routes.New()
+	m.LogMiddlewares(e)
 	e.Logger.Fatal(e.Start(":8000"))
 }
